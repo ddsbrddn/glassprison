32d8870ae449cf895c29d4312893c203c4bb19e0
Dodi Sabaruddin <ddsbrddn@gmail.com>
2015-08-18 19:22:52 +0700

    Add user micropost - image upload in production

--- config/routes.rb
+++ config/routes.rb
@@ -9,6 +9,8 @@ Rails.application.routes.draw do
   get 'signup'  => 'users#new'
   get    'login'   => 'sessions#new'
   post   'login'   => 'sessions#create'
-  delete 'logout'  => 'sessions#destroy'  
+  delete 'logout'  => 'sessions#destroy'
   resources :users
+
+  resources :microposts, only: [:create, :destroy]
 end
