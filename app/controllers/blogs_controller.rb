ee34477f8f11cf8b80dc7ad4f5d093dfd959a3cc
Dodi Sabaruddin <ddsbrddn@gmail.com>
2015-10-19 18:29:26 +0700

    Edit and Delete blog posts ability

--- app/controllers/blogs_controller.rb
+++ app/controllers/blogs_controller.rb
@@ -20,6 +20,27 @@ class BlogsController < ApplicationController
 		@blog = Blog.find(params[:id])
 	end
 
+	def edit
+		@blog = Blog.find(params[:id])
+	end
+
+	def update
+		@blog = Blog.find(params[:id])
+
+		if @blog.update(params[:blog].permit(:title, :body))
+			redirect_to @blog
+		else
+			render 'edit'
+		end
+	end
+
+	def destroy
+		@blog = Blog.find(params[:id])
+		@blog.destroy
+
+		redirect_to blogs_path
+	end
+
 	private
 	def blog_params
 		params.require(:blog).permit(:title, :body)
