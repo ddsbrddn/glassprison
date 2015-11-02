2044d0470772595d9969f8e382d77351525ecec5
Dodi Sabaruddin <ddsbrddn@gmail.com>
2015-11-02 22:52:42 +0700

    Add cart

--- app/models/line_item.rb
+++ app/models/line_item.rb
@@ -1,8 +1,13 @@
 class LineItem < ActiveRecord::Base
-	belongs_to :order
+	#belongs_to :order
 	belongs_to :product
 	belongs_to :cart
 	def total_price
 		product.price * quantity
 	end
+
+	def quantity
+    @cart = Cart.all.count
+  end
+
 end
